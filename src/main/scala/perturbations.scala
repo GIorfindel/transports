4a3003dc966780bedd2b7cb4ed0b47ab82753369
ChristGood <christ.good@yahoo.fr>
2017-01-18 23:50:31 +0100

    Essaies avec l'api de navitia et authentification

--- src/main/scala/perturbations.scala
+++ src/main/scala/perturbations.scala
@@ -16,44 +16,67 @@ import scala.concurrent.duration._
 import java.util.concurrent.TimeUnit
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.headers._
 
 object Perturbations {
 
-  final case class Meta(version: String, date: String, call: String)
-  final case class Metro(line: String, slug: String, title: String, message: String)
-  final case class Rer(line: String, slug: String, title: String, message: String)
-  final case class Tramway(line: String, slug: String, title: String, message: String)
-  final case class Response(metros: List[Metro], rers: List[Rer], tramways: List[Tramway])
-  final case class Traffic(response: Response, _meta: Meta)
+  object TrafficRATP {
+    final case class Meta(version: String, date: String, call: String)
+    final case class Metro(line: String, slug: String, title: String, message: String)
+    final case class Rer(line: String, slug: String, title: String, message: String)
+    final case class Tramway(line: String, slug: String, title: String, message: String)
+    final case class Response(metros: List[Metro], rers: List[Rer], tramways: List[Tramway])
+    final case class Traffic(response: Response, _meta: Meta)
 
-  implicit val metaFormat = jsonFormat3(Meta)
-  implicit val metroFormat = jsonFormat4(Metro)
-  implicit val rerFormat = jsonFormat4(Rer)
-  implicit val tramwayFormat = jsonFormat4(Tramway)
-  implicit val responseFormat = jsonFormat3(Response)
-  implicit val trafficFormat = jsonFormat2(Traffic)
+    implicit val metaFormat = jsonFormat3(Meta)
+    implicit val metroFormat = jsonFormat4(Metro)
+    implicit val rerFormat = jsonFormat4(Rer)
+    implicit val tramwayFormat = jsonFormat4(Tramway)
+    implicit val responseFormat = jsonFormat3(Response)
+    implicit val trafficFormat = jsonFormat2(Traffic)
+  }
+
+  object LignesIDF {
+    final case class Type(name: String)
+    final case class Ligne(code: String, name: String, physical_modes: List[Type], text_color: String, color: String, id: String)
+    final case class Reseau(lines: List[Ligne])
+
+    implicit val typeFormat = jsonFormat1(Type)
+    implicit val ligneFormat = jsonFormat6(Ligne)
+    implicit val reseauFormat = jsonFormat1(Reseau)
+
+  }
 
   def main(args: Array[String]) {
+    // TESTS
+    //import TrafficRATP._
+    import LignesIDF._
 
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
 
     val duration = Duration(15000, MILLISECONDS)
+    val cred = BasicHttpCredentials("232e61e7-8b77-4a6b-8bc2-7b6dd2732d37", "")
+    val authorization = headers.Authorization(BasicHttpCredentials("232e61e7-8b77-4a6b-8bc2-7b6dd2732d37", ""))
 
     val responseFuture: Future[HttpResponse] =
-      Http().singleRequest(HttpRequest(uri = "https://api-ratp.pierre-grimaud.fr/v2/traffic/"))
+      Http().singleRequest(HttpRequest(
+        uri = "https://api.navitia.io/v1/coverage/fr-idf/lines?count=1000&start_page=0",
+        headers = List(authorization)
+      ))
+    //Http().singleRequest(HttpRequest(uri = "https://api-ratp.pierre-grimaud.fr/v2/traffic/")) //Traffic RATP
+
     val result = Await.result(responseFuture, duration).asInstanceOf[HttpResponse]
     result._1.intValue() match {
       case 200 => {
         println(result.entity)
-        val ticker = Unmarshal(result.entity).to[Traffic]
+        //  val ticker = Unmarshal(result.entity).to[Traffic] //Traffic RATP
+        val ticker = Unmarshal(result.entity).to[Reseau]
         val t = Await.result(ticker, 10.second)
         println(t)
       }
       case 500 => println("Erreur du serveur, veuillez réessayer")
     }
-    //complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, responseFuture.toString()))
-
   }
 }
